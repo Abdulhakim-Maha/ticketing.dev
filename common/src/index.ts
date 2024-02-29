e56cec63eb2bef8eef17494e5ca907f4c542f6da
Abdulhakim-Maha <63011075@kmitl.ac.th>
2024-02-29 14:10:48 +0700

    addition common

---
+++ common/src/index.ts
@@ -0,0 +1,16 @@
+interface Color {
+  red: number;
+  blue: number;
+  green: number;
+  alpha: number;
+}
+
+const color: Color = {
+  red: 10,
+  blue: 10,
+  green: 10,
+  alpha: 0.5
+}
+console.log(color)
+
+export default color;

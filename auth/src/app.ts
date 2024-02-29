fcb32137d07f3de90116dc56ea3e96498761cdba
Abdulhakim-Maha <63011075@kmitl.ac.th>
2024-02-29 14:43:08 +0700

    addition common

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -7,8 +7,7 @@ import { currentUserRouter } from './routes/current-user'
 import { signinRouter } from './routes/signin'
 import { signoutRouter } from './routes/singout'
 import { signupRouter } from './routes/signup'
-import { errorHandler } from './middlewares/error-handler'
-import { NotFoundError } from './errors/not-found-error'
+import { errorHandler, NotFoundError } from '@xdticket/common'
 
 const app = express()
 app.set('trust proxy', true)
